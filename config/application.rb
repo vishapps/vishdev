c6557e88e0a635a6b2491b067992c53de448c67d
adhikari <vish.dba1@gmail.com>
2016-05-06 19:50:46 +0000

    Updated Invalid CSS quotes: &&#8220 &#8220

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Workspace
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.serve_static_assets = true
-    #config.active_record.raise_in_transactional_callbacks = true
+    config.active_record.raise_in_transactional_callbacks = true
     
   end
 end
