54231c3175826e258084589885df5a82e0d565b9
adhikari <vish.dba1@gmail.com>
2016-05-06 15:43:36 +0000

    config.assets update for heroku

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,8 @@ module Workspace
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = false
+    config.assets.initialize_on_precompile = false
+    config.active_record.raise_in_transactional_callbacks = true
     
   end
 end
