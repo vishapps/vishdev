ef89a9202b46dd985029250e8709500ba87888f6
adhikari <vish.dba1@gmail.com>
2016-05-07 06:27:50 +0000

    added sendEmail.php

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,6 @@ module Workspace
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.serve_static_assets = true
     config.active_record.raise_in_transactional_callbacks = true
-    
+    config.assets.initialize_on_precompile = false
   end
 end
